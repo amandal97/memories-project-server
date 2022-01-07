a02685fb0980578a7583f45587a747b19177030a
amandal97 <mandaline97@gmail.com>
2022-01-07 22:03:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRouter from "./routes/posts.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// use routes
+app.use("/posts", postRouter);
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening to port ${PORT}!`))
+  )
+  .catch((err) => console.log(err.message));
